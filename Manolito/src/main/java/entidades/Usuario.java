d51a1ccf09c769243704aad7becdd250225b18b7
Kobokila <arodriguez2910@hotmail.es>
2019-03-04 15:31:30 +0100

    Entidades correctas

--- Manolito/src/main/java/entidades/Usuario.java
+++ Manolito/src/main/java/entidades/Usuario.java
@@ -1,5 +1,121 @@
 package entidades;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USUARIOS", schema="PROYECTO")
 public class Usuario {
+	
+	@Id
+	@Column(name="DAS", unique=true, nullable=false)
+	private int das;
+	
+	@Column(name="NOMBRE")
+	private String nombre;
+	
+	@Column(name="APELLIDO")
+	private String apellido;
+	
+	@Column(name="PASSWORD", nullable=false)
+	private String password;
+	
+	@Column(name="ROL", nullable=false)
+	private String rol;
+	
+	@Column(name="PASSCAMBIADA", nullable=false)
+	private boolean passcambiada;
+
+	public Usuario() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the das
+	 */
+	public int getDas() {
+		return das;
+	}
+
+	/**
+	 * @param das the das to set
+	 */
+	public void setDas(int das) {
+		this.das = das;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the apellido
+	 */
+	public String getApellido() {
+		return apellido;
+	}
+
+	/**
+	 * @param apellido the apellido to set
+	 */
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the rol
+	 */
+	public String getRol() {
+		return rol;
+	}
+
+	/**
+	 * @param rol the rol to set
+	 */
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+	/**
+	 * @return the passcambiada
+	 */
+	public boolean isPasscambiada() {
+		return passcambiada;
+	}
+
+	/**
+	 * @param passcambiada the passcambiada to set
+	 */
+	public void setPasscambiada(boolean passcambiada) {
+		this.passcambiada = passcambiada;
+	}
+	
+	
 
 }
