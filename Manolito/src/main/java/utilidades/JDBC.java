e43fdc6be67b4c7e14411e075ebb971cb8c5372a
alvaroortegasanchezcuesta <47524330+alvaroortegasanchezcuesta@users.noreply.github.com>
2019-03-05 09:14:35 +0100

    Actualizo el archivo JDBC.java con la conexion correcta

--- Manolito/src/main/java/utilidades/JDBC.java
+++ Manolito/src/main/java/utilidades/JDBC.java
@@ -4,7 +4,7 @@ public interface JDBC {
 
 	String DRIVERS = "oracle.jdbc.driver.OracleDriver";
 	String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-	String USER = "alvaro";
-	String PASSWORD = "alvaro123";
+	String USER = "proyecto";
+	String PASSWORD = "proyecto";
 	
 }
