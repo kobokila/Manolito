8a0ec19df43ef363f59d25bdb67b66554ace0739
alvaroortegasanchezcuesta <47524330+alvaroortegasanchezcuesta@users.noreply.github.com>
2019-03-05 09:10:03 +0100

    Añado clase para conexion

---
+++ Manolito/src/main/java/utilidades/JDBC.java
@@ -0,0 +1,10 @@
+package utilidades;
+
+public interface JDBC {
+
+	String DRIVERS = "oracle.jdbc.driver.OracleDriver";
+	String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	String USER = "alvaro";
+	String PASSWORD = "alvaro123";
+	
+}
